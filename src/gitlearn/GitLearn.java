20d72c34417e04ec68057bd3dde914a19a452f8f
nbhir <nbhir@nbhirudXPS>
2017-02-08 19:35:27 -0500

    netbeans commit 1.1

---
+++ src/gitlearn/GitLearn.java
@@ -0,0 +1,17 @@
+package gitlearn;
+
+/**
+ *
+ * @author nbhirud
+ */
+public class GitLearn {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Git");
+    }
+    
+}
